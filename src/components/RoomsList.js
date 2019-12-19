75d798954099e0a6c883fc29194172575129a0e3
alshafi834 <alshafi834@gmail.com>
2019-12-19 21:23:48 +0100

    room filter in progress

---
+++ src/components/RoomsList.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Rooms from "./Rooms";
+
+export default function RoomsList({ rooms }) {
+  if (rooms.length === 0) {
+    return (
+      <div className="empty-search">
+        <h3>Unfortunately no rooms matched with your filter</h3>
+      </div>
+    );
+  }
+  return (
+    <section className="roomslist">
+      <div className="roomslist-center">
+        {rooms.map(item => {
+          return <Rooms key={item.id} room={item} />;
+        })}
+      </div>
+    </section>
+  );
+}
