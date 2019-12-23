3cd64e9c47468d018104dd8fa56dfccb70a21373
alshafi834 <alshafi834@gmail.com>
2019-12-23 20:47:14 +0100

    footer added to all page

--- src/pages/Rooms.js
+++ src/pages/Rooms.js
@@ -2,6 +2,7 @@ import React from "react";
 import Hero from "../components/Hero";
 import Banner from "../components/Banner";
 import RoomsContainer from "../components/RoomsContainer";
+import Footer from "../components/Footer";
 import { Link } from "react-router-dom";
 
 const Rooms = () => {
@@ -15,6 +16,7 @@ const Rooms = () => {
         </Banner>
       </Hero>
       <RoomsContainer />
+      <Footer></Footer>
     </>
   );
 };
